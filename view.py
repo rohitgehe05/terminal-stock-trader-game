3cfbf4e02e87eae2ab852569dc81a2345ab2b98e
Rohit Gehe <rohit.gehe@efficientbazaar.com>
2018-01-08 01:15:28 +0530

    connected stock_search function to login_menu, added getpass for hidden password input for user, refactored buy and sell stock to comm. with controller instead of wrapper directly

--- view.py
+++ view.py
@@ -2,7 +2,9 @@ import controller
 from wrapper import Lookup
 from colorama import init, Fore, Style
 from prettytable import PrettyTable
+from IPython import embed
 import os
+import getpass
 
 
 init(autoreset=True)
@@ -28,7 +30,7 @@ def main_menu():
         if isinstance(user_input, int) == True:
             if user_input == 1:
                 username = str(input('\nPlease enter your username: '))
-                password = str(input('Please enter your password: '))
+                password = str(getpass.getpass('Please enter your password: '))
                 if controller.login(username, password) == 1:
                     print(Fore.GREEN +
                           Style.BRIGHT + '\nLogin successful!')
@@ -74,7 +76,9 @@ def login_menu(username):
             # check portfolio
             pass
         elif user_input == 2:
-            # go to stock search menu
+            company = str(
+                input("\nEnter the name of the company you want to search for: "))
+            stock_search(company)
             pass
         elif user_input == 3:
             buy_stock(username)
@@ -90,22 +94,35 @@ def view_portfolio():
     pass
 
 
-def stock_search():
-    pass
+def stock_search(company):
+    # var initiation
+    t = PrettyTable()
+    # print table
+    if controller.company_search(company) != 0:
+        t.field_names = controller.company_search(company).keys()
+        t.add_row(controller.company_search(company).values())
+        print('\n', t)
+    else:
+        print(Fore.RED + Style.BRIGHT +
+              '\nWhoops! We could not find the company named "' + company + '"')
 
 
 def buy_stock(username):
-    q = Lookup()
+    # var initiation
     t = PrettyTable()
+    print()
     symbol = str(input("Enter the ticker symbol for the company: ")).upper()
     qty = int(input("Enter the quantity to buy: "))
-    t.field_names = q.get_details(symbol).keys()
-    t.add_row(q.get_details(symbol).values())
-    print(t.get_string(fields=["Name", "Symbol", "LastPrice", "Change",
-                               "ChangePercent", "ChangeYTD", "ChangePercentYTD", "Volume"]) + '\n')
-    print('Your total purchase value is ${}'.format(q.get_quote(symbol) * qty))
+    # print table
+    t.field_names = controller.get_details(symbol).keys()
+    t.add_row(controller.get_details(symbol).values())
+    print('\n' + t.get_string(fields=["Name", "Symbol", "LastPrice", "Change",
+                                      "ChangePercent", "ChangeYTD", "ChangePercentYTD", "Volume"]) + '\n')
+    print('Your total purchase value is ${0:.2f}'.format(
+        controller.get_quote(symbol) * qty))
     confirmation = str(input(
         'Are you sure you want to go ahead with this transaction? (Y/N) '))
+    # validate confirmation and do actual buying of stock
     if confirmation != 'Y' and confirmation != 'y':
         print(Fore.RED + Style.BRIGHT + '\nINVALID OPTION, TRY AGAIN!\n')
     else:
@@ -120,25 +137,40 @@ def buy_stock(username):
             print('-' * 30)
             print('You have successfully purchased {} shares of {} at ${} per share.'.format(
                 qty, symbol, last_price))
-            print('For a total of ${}'.format(total_price))
-            print('Your balance is now ${}.'.format(balance))
+            print('For a total of ${0:.2f}'.format(total_price))
+            print('Your balance is now ${0:.2f}.'.format(balance))
 
 
 def sell_stock(username):
-    # to-do: create confirmation loop(?), show table of stock data(?)
+    # var initiation
+    t = PrettyTable()
     symbol = str(input("Enter the ticker symbol for the company: ")).upper()
     qty = int(input("Enter the quantity to sell: "))
-    last_price, total_price, balance, response = controller.sell_stock(
-        symbol, qty, username)
-    if response == 400:
-        print('The quantity entered exceeds the quantity held in the portfolio! Please try again.')
-    elif response == 401:
-        print('Your portfolio does not have this stock! Please try again.')
-    elif response == 500:
-        print("There is a connectivity issue! Please try again.")
-    elif response == 200:
-        print('-' * 30)
-        print('You have successfully sold {} shares of {} at ${} per share.'.format(
-            qty, symbol, last_price))
-        print('For a total of ${}'.format(total_price))
-        print('Your balance is now ${}.'.format(balance))
+    # print table
+    t.field_names = controller.get_details(symbol).keys()
+    t.add_row(controller.get_details(symbol).values())
+    print('\n' + t.get_string(fields=["Name", "Symbol", "LastPrice", "Change",
+                                      "ChangePercent", "ChangeYTD", "ChangePercentYTD", "Volume"]) + '\n')
+    print('Your total purchase value is ${0:.2f}'.format(
+        controller.get_quote(symbol) * qty))
+    confirmation = str(input(
+        'Are you sure you want to go ahead with this transaction? (Y/N) '))
+    # validate confirmation and do actual buying of stock
+    if confirmation != 'Y' and confirmation != 'y':
+        print(Fore.RED + Style.BRIGHT + '\nINVALID OPTION, TRY AGAIN!\n')
+    else:
+        last_price, total_price, balance, response = controller.sell_stock(
+            symbol, qty, username)
+        if response == 400:
+            print(
+                'The quantity entered exceeds the quantity held in the portfolio! Please try again.')
+        elif response == 401:
+            print('Your portfolio does not have this stock! Please try again.')
+        elif response == 500:
+            print("There is a connectivity issue! Please try again.")
+        elif response == 200:
+            print('-' * 30)
+            print('You have successfully sold {} shares of {} at ${} per share.'.format(
+                qty, symbol, last_price))
+            print('For a total of ${0:.2f}'.format(total_price))
+            print('Your balance is now ${0:.2f}.'.format(balance))
